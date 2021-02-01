ac6731bb144e44a6f20e541bf79e0ac8ea6480f0
weilinfox <weilin.king@qq.com>
2021-02-01 23:32:26 +0800

    add wiringX python2 version

--- raspi_client_wiringX_python2.py
+++ raspi_client_wiringX_python2.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
 # need root
-# wiringX 版本
+# wiringX python2 version
 from wiringX import gpio
-import subprocess, time, os
+import commands, time, os
 import requests
 import traceback
 
@@ -14,15 +14,14 @@ cores = int(os.system('cat /proc/cpuinfo| grep "processor"| wc -l'))
 # raspberrypi3
 gpio.setup(gpio.RASPBERRYPI3)
 # GPIO.1 PIN12
-# wiringX 编码同 wiringPi
+# wiringX code just like wiringPi
 FAN_GPIO = gpio.PIN1
 fan_status = 0
 # set FAN_GPIO output
 gpio.pinMode(FAN_GPIO, gpio.PINMODE_OUTPUT)
 
-# 状态上报
-URL = '127.0.0.1'
-PORT = 8080
+# upload status
+URL = 'http://127.0.0.1:8080'
 NAME = 'raspi'
 
 # files
@@ -31,7 +30,7 @@ netFile = '/proc/net/dev'
 statusCom = 'w'
 ditlCom = 'vmstat'
 
-# 同样的错误半小时上报一次
+# the same error report once in half an hour
 #gpio_error_time = 0
 #temp_exceed_time = 0
 
@@ -39,14 +38,14 @@ reportDelay = 60
 
 while True:
     try:
-        resp = requests.post(url=f'http://{URL}:{PORT}', json={'post_type':'haku-manager','server_name':NAME,'message_type':'heartBeat'})
+        resp = requests.post(url=URL, json={'post_type':'haku-manager','server_name':NAME,'message_type':'heartBeat'})
     except Exception as e:
         print(e)
     else:
         if resp.status_code != 200:
-            print(f'Send heartBeat returned: {resp.status_code}')
+            print('Send heartBeat returned: ' + str(resp.status_code))
     reportDelay += 1
-    # 获取CPU温度
+    # get CPU temperature
     try:
         tempF = open(tempFile, 'r')
         tempData = tempF.read()
@@ -56,14 +55,14 @@ while True:
         msg = traceback.format_exc()
         print(msg)
         try:
-            requests.post(url=f'http://{URL}:{PORT}', json={'post_type':'haku-manager','server_name':NAME,'message_type':'error','message':'Get pi temperature failed!\n'+msg})
+            requests.post(url=URL, json={'post_type':'haku-manager','server_name':NAME,'message_type':'error','message':'Get pi temperature failed!\n'+msg})
         except Exception as e:
             print(e)
     finally:
         cpu_temp = int(tempData) / 1000
 
     try:
-        # low 为启动 high 为关闭
+        # low on, high off
         if not fan_status and (cpu_temp >= 50.0 or cpu_temp < 0):
             gpio.digitalWrite(FAN_GPIO, gpio.LOW)
             fan_status = 1
@@ -73,20 +72,20 @@ while True:
     except:
         msg = traceback.format_exc()
         print(msg)
-        requests.post(url=f'http://{URL}:{PORT}', json={'post_type':'haku-manager','server_name':NAME,'message_type':'error','message':'Switch fan failed!\n'+msg})
+        requests.post(url=URL, json={'post_type':'haku-manager','server_name':NAME,'message_type':'error','message':'Switch fan failed!\n'+msg})
 
-    # status 上报 10min
+    # status upload duration:10min
     if reportDelay >= 60:
         reportDelay = 0
         reportDict = {'post_type':'haku-manager','server_name':NAME,'message_type':'status','status':{'time':{'uptime':''}, 'temp':{'cpu_temp':cpu_temp, 'sys_temp':cpu_temp, 'fan_status':fan_status}, 'cpu':{'cpu_cores':cores, 'load_average':0.0, 'wa':0}, 'disk':{'bi':0, 'bo':0}, 'memory':{'free':0, 'buff':0, 'cache':0}, 'swap':{'si':0, 'so':0}, 'process':{'r':0, 'b':0}, 'net':{}}}
         try:
-            wMsg = list(subprocess.getoutput(statusCom).split())
+            wMsg = list(commands.getoutput(statusCom).split())
             if len(wMsg) > 8:
                 uptime = ''
                 getU = False
                 for s in wMsg:
                     if getU and uptime and s[-1] != ',': break
-                    elif getU: uptime += f' {s}'
+                    elif getU: uptime += ' ' + s
                     if s == 'up': getU = True
                 reportDict['status']['time']['uptime'] = uptime[:-1].strip()
                 getU = False
@@ -95,7 +94,7 @@ while True:
                         reportDict['status']['cpu']['load_average'] =  float(s[:-1])
                         break
                     if s == 'average:': getU = True
-            statMsg = list(subprocess.getoutput(ditlCom).split())
+            statMsg = list(commands.getoutput(ditlCom).split())
             if len(statMsg) == 40:
                 reportDict['status']['process']['r'] = int(statMsg[23])
                 reportDict['status']['process']['b'] = int(statMsg[24])
@@ -120,8 +119,7 @@ while True:
         else:
             #print(reportDict)
             try:
-                requests.post(url=f'http://{URL}:{PORT}', json=reportDict)
+                requests.post(url=URL, json=reportDict)
             except Exception as e:
                 print(e)
-    time.sleep(10)
-
+    time.sleep(10) 
